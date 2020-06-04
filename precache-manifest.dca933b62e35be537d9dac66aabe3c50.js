1c137614ae1fbc0d237d531c70733b3e3cd9c025
Wayne Li <wyl6@cornell.edu>
2020-06-03 20:59:26 -0400

    Updates

---
+++ precache-manifest.dca933b62e35be537d9dac66aabe3c50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4efd60d25802075cff319b06c87ca7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5da89c9a967b87384e0a",
+    "url": "/static/css/main.19158211.chunk.css"
+  },
+  {
+    "revision": "ca65915cedcffddb20d2",
+    "url": "/static/js/2.c3c61742.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c3c61742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5da89c9a967b87384e0a",
+    "url": "/static/js/main.aa1cc215.chunk.js"
+  },
+  {
+    "revision": "153a23f942012046af99",
+    "url": "/static/js/runtime-main.ea9c13c0.js"
+  }
+]);
