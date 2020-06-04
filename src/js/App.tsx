3705c51b30f7530b0924fa87b06a0394ad5f3f4c
Wayne Li <wyl6@cornell.edu>
2020-06-03 22:27:30 -0400

    missing src files

---
+++ src/js/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from '../css/logo.svg';
+import '../css/App.css';
+import AnimatedName from './AnimatedName';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AnimatedName />
+      </header>
+    </div>
+  )
+}
+
+export default App;
