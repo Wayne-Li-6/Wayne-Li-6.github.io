8b445a3bcc7f4ac9eabee6efbb8f0a379e9394a5
Wayne Li <wyl6@cornell.edu>
2020-06-11 23:02:53 -0400

    Basic functionality done

--- src/js/App.tsx
+++ src/js/App.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
-import '../css/App.css';
-import AnimatedName from './AnimatedName';
+import { Switch, Route } from 'react-router-dom';
+import '../css/app.css';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <AnimatedName />
-      </header>
-    </div>
-  )
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Switch>
+                    <Route path="/contact" component={Contact} />
+                    <Route path="/about" component={About} />
+                    <Route exact path="/" component={Home} />
+                </Switch>
+            </header>
+        </div>
+    )
 }
 
 export default App;
